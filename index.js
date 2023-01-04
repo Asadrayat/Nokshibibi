3a41edc96a9cb41bcfbfea375af431693c253436
Asadrayat <asad.rayat4@gmail.com>
2023-01-04 15:09:44 +0600

    added indexjs

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// Database Connection
+const uri = "mongodb+srv://airCnc:xreepG7ypioRcLda@cluster0.nkyrz6w.mongodb.net/?retryWrites=true&w=majority";
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    const homesCollection = client.db('aircncdb').collection('homes')
+    const usersCollection = client.db('aircncdb').collection('users')
+
+    // save user & generate jwt 
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const filter = { email: email }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options)
+      console.log(result)
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d',
+      })
+      console.log(token)
+      res.send({ result, token })
+    })
+    console.log('Database Connected...')
+  } finally {
+  }
+}
+
+run().catch(err => console.error(err))
+
+app.get('/', (req, res) => {
+  res.send('Server is running...')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running...on ${port}`)
+})
